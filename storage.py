2fc9447a389b95c93da4211b23ef2ab4a3c434d2
nanachiwinters <nanachi.winters@gmail.com>
2026-07-13 23:24:16 -0700

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,17 @@
+import json
+import os
 
+COINS_FILE = "coins.json"
+
+
+def load_coins():
+    if not os.path.exists(COINS_FILE):
+        return {}
+
+    with open(COINS_FILE, "r") as f:
+        return json.load(f)
+
+
+def save_coins(data):
+    with open(COINS_FILE, "w") as f:
+        json.dump(data, f, indent=4)
