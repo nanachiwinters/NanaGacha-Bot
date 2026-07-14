8cfb68d86796e856d3cbc9aa11f06e6d89ba1f00
nanachiwinters <nanachi.winters@gmail.com>
2026-07-13 22:37:33 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import discord
+from discord import app_commands
 
+from config import TOKEN
+
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print(f"✅ Logged in as {client.user}")
+
+
+client.run(TOKEN)
