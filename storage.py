50b62b6a10d2fd65150c8773275ea39fb00db8d2
nanachiwinters <nanachi.winters@gmail.com>
2026-07-14 00:41:27 -0700

    Update storage.py

--- storage.py
+++ storage.py
@@ -15,3 +15,16 @@ def load_coins():
 def save_coins(data):
     with open(COINS_FILE, "w") as f:
         json.dump(data, f, indent=4)
+
+
+def load_roles():
+    try:
+        with open("roles.json", "r") as f:
+            return json.load(f)
+    except:
+        return {}
+
+
+def save_roles(data):
+    with open("roles.json", "w") as f:
+        json.dump(data, f, indent=4)
