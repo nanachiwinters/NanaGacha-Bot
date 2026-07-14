760066373b3d79e0cd2f5f703e39616f9539dd9b
nanachiwinters <nanachi.winters@gmail.com>
2026-07-14 02:42:35 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -512,6 +512,7 @@ class MyRoleMenu(discord.ui.View):
     name="setup",
     description="Creates or updates the Nachi menu."
 )
+@app_commands.default_permissions(administrator=True)
 async def setup(interaction: discord.Interaction):
 
     embed = discord.Embed(
@@ -546,36 +547,6 @@ async def setup(interaction: discord.Interaction):
         "✅ Nachi menu created!",
         ephemeral=True
         
-    )
-
-# -----------------------------
-# TEMP HERALD COMMAND
-# -----------------------------
-
-@tree.command(
-    name="herald",
-    description="Temporary command"
-)
-async def herald(interaction: discord.Interaction):
-    print("HERALD COMMAND RAN")
-
-    role = discord.utils.get(
-        interaction.guild.roles,
-        name="🛡️Herald of Chaos🛡️"
-    )
-
-    if role is None:
-        await interaction.response.send_message(
-            "❌ Role not found.",
-            ephemeral=True
-        )
-        return
-
-    await interaction.user.add_roles(role)
-
-    await interaction.response.send_message(
-        "✅ Done!",
-        ephemeral=True
     )
 
 
