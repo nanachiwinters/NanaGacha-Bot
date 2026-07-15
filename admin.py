af5a0b6b8347c5f5fa5f9df71b06fbc4ab50cc14
nanachiwinters <nanachi.winters@gmail.com>
2026-07-14 21:01:58 -0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,12 @@
+import discord
+from discord import app_commands
 
+def setup_admin(tree):
+
+    @tree.command(
+        name="admin",
+        description="Open the admin panel."
+    )
+    @app_commands.default_permissions(administrator=True)
+    async def admin(interaction: discord.Interaction):
+        ...
